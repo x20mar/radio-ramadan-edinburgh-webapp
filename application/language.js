62829d727d4e6a469511079b8251d55bdf79bde7
Omar Ali <oali@flexiant.com>
2014-04-30 14:09:16 +0100

    #1 Added full language file

--- application/language.js
+++ application/language.js
@@ -2,9 +2,15 @@
 namespacer(window, {RRE: {language: { title:{}, message:{}}}});
 
 (function(namespace, $) {
+    namespace.MENU = "Menu";
     namespace.LIVE = "Live";
     namespace.LISTEN_AGAIN = "Listen Again";
     namespace.SCHEDULE = "Schedule";
 })(RRE.language.title, jQuery);
 
-
+(function(namespace, $) {
+    namespace.ON_NOW = "On Now";
+    namespace.ON_NEXT = "On Next";
+    namespace.LISTEN_AGAIN_TO_PAST_SHOWS = "Listen Again to past shows";
+    namespace.COMING_UP = "See what shows are coming up on Radio Ramadan";
+})(RRE.language.message, jQuery);
--- application/main.js
+++ application/main.js
@@ -26,9 +26,13 @@ rreApp.config(function($routeProvider) {
 
 rreApp.factory('Page', function() {
     var title = RRE.language.title.LIVE;
+    var menu = RRE.language.title;
     return {
         title: function() {
             return title;
+        },
+        menu: function() {
+            return menu;
         },
         setTitle: function(newTitle) {
             title = newTitle;
@@ -41,7 +45,8 @@ rreApp.controller('mainController', function($scope, Page) {
     // create a message to display in our view
     $scope.Page = Page;
     Page.setTitle(RRE.language.title.LIVE);
-    $scope.message = 'On Next:Program 2';
+    $scope.onNow = RRE.language.message.ON_NOW+": " +"Program 1";
+    $scope.onNext = RRE.language.message.ON_NEXT+": " +"Program 2";
 
     $scope.snapOpts = {
         disable: 'right'
@@ -56,7 +61,7 @@ rreApp.controller('mainController', function($scope, Page) {
 
 rreApp.controller('listenagainController', function($scope, Page) {
     Page.setTitle(RRE.language.title.LISTEN_AGAIN);
-    $scope.message = 'Listen Again to past shows';
+    $scope.message = RRE.language.message.LISTEN_AGAIN_TO_PAST_SHOWS;
 });
 
 rreApp.controller('scheduleController', function($scope, Page) {
@@ -89,5 +94,5 @@ rreApp.controller('scheduleController', function($scope, Page) {
     };
     /* event sources array*/
     $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
-    $scope.message = 'See what shows are comming up on Radio Ramadan';
+    $scope.message = RRE.language.message.COMING_UP;
 });
