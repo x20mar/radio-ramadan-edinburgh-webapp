d031ffd97b815f56dc37c639e7fcd97870e8aff7
Omar Ali <oali@flexiant.com>
2014-05-25 23:23:35 +0100

    #1 grunt package mostly works. Issue in plangular because of the way it is accessed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,71 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+        temp: {
+            src: [ 'temp/*' ]
+        },
+        dist: {
+            src: [ 'build/*' ]
+        },    
+    },
+    copy: {
+        dist: {
+            files: [  
+                {expand : true, dest : 'build/application/img', cwd : 'src/application/img', src : ['**/*.png']},
+                {expand : true, dest : 'build/application/fonts', cwd : 'src/libs/fonts', src : ['**/*.*']},
+            ]
+        }
+    },
+    concat_css: {
+        all: {
+            src: ['src/libs/*.css', 'src/application/*.css'], dest: 'build/application/compiled.css'
+        },
+        options: {
+            compress:true,
+        }
+    },
+    processhtml: {
+        dist: {
+            files: {
+                'build/index.html': ['src/index.html']
+            }
+        }
+    },
+    ngmin: {
+        controllers: {
+            src: ['src/application/main.js'],
+            dest: 'temp/main.js'
+        },
+    },
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
+        dist: {
+            files: {
+                'build/application/compiled.js': ['src/libs/jquery.min.js', 'src/libs/jquery-ui.custom.min.js', 'src/libs/fullcalendar.min.js', 'src/libs/gcal.js', 'src/libs/snap.min.js', 'src/libs/angular.min.js', 'src/libs/angular-bootstrap.min.js', 'src/libs/angular-route.min.js', 'src/libs/angular-snap.min.js', 'src/libs/angular-calendar.js', 'src/libs/angular-media-player.min.js', 'src/libs/plangular.js', 'temp/main.js']
+            }
+        }
+    },
+    compress: {
+        main: {
+            options: {
+                mode: 'gzip'
+            },
+        files: [
+            // Each of the files in the src/ folder will be output to
+            // the dist/ folder each with the extension .gz.js
+            {expand: true, cwd: 'temp/', src: ['compiled.js'], dest: 'build/application', ext: '.gz.js'},
+            {expand: true, cwd: 'temp/', src: ['compiled.css'], dest: 'build/application', ext: '.gz.css'}
+        ]
     }
+}
   });
-
-  // Load the plugin that provides the "uglify" task.
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-concat-css');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-ngmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  // Default tasks.
+  grunt.registerTask('default', ['clean', 'copy', 'ngmin', 'uglify', 'concat_css', 'processhtml']);
 };
