7214710f214ce544baab2d70dc9558a998a01e44
Omar Ali <oali@flexiant.com>
2014-06-08 18:05:15 +0100

    Testing new gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,131 +1,133 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    clean: {
-        temp: {
-            src: [ 'temp/*' ]
-        },
-        dist: {
-            src: [ 'cordivacli-build/www/*' ]
-        },    
-    },
-    copy: {
-        dist: {
-            files: [  
-                {expand : true, dest : 'cordivacli-build/www/application/img', cwd : 'src/application/img', src : ['**/*.png']},
-                {expand : true, dest : 'cordivacli-build/www/application/fonts', cwd : 'src/libs/fonts', src : ['**/*.*']},
-            ]
-        }
-    },
-    cssmin: {
-        combine: {
-            files: {
-                'cordivacli-build/www/application/compiled.css': ['src/libs/*.css', 'src/application/*.css']
-            }
-        }
-    },
-    processhtml: {
-        dist: {
-            files: {
-                'cordivacli-build/www/index.html': ['src/index.html']
-            }
-        }
-    },
-    ngmin: {
-        main: {
-            src: ['src/application/main.js'],
-            dest: 'temp/main.js'
-        },
-        plangular: {
-            src: ['src/libs/plangular.js'],
-            dest: 'temp/plangular.js'
-        },
-    },
-    uglify: {
-        dist: {
-            files: {
-                'cordivacli-build/www/application/compiled.js': ['src/libs/jquery.min.js', 'src/libs/jquery-ui.custom.min.js', 'src/libs/fullcalendar.min.js', 'src/libs/gcal.js', 'src/libs/snap.min.js', 'src/libs/angular.min.js', 'src/libs/angular-bootstrap.min.js', 'src/libs/angular-route.min.js', 'src/libs/angular-snap.min.js', 'src/libs/angular-calendar.js', 'src/libs/angular-media-player.min.js', 'temp/plangular.js', 'temp/main.js']
-            }
-        }
-    },
-    compress: {
-        main: {
-            options: {
-                mode: 'gzip'
+    grunt.initConfig({
+        clean: {
+            temp: {
+                src: [ 'temp/*' ]
             },
-        files: [
-            // Each of the files in the src/ folder will be output to
-            // the dist/ folder each with the extension .gz.js
-            {expand: true, cwd: 'temp/', src: ['compiled.js'], dest: 'cordivacli-build/www/application', ext: '.gz.js'},
-            {expand: true, cwd: 'temp/', src: ['compiled.css'], dest: 'cordivacli-build/www/application', ext: '.gz.css'}
-        ]
+            dist: {
+                src: [ 'cordivacli-build/www/*' ]
+            },    
+        },
+        copy: {
+            dist: {
+                files: [  
+                    {expand : true, dest : 'cordivacli-build/www/application/img', cwd : 'src/application/img', src : ['**/*.png']},
+                    {expand : true, dest : 'cordivacli-build/www/application/fonts', cwd : 'src/libs/fonts', src : ['**/*.*']},
+                ]
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'cordivacli-build/www/application/compiled.css': ['src/libs/*.css', 'src/application/*.css']
+                }
+            }
+        },
+        processhtml: {
+            dist: {
+                files: {
+                    'cordivacli-build/www/index.html': ['src/index.html']
+                }
+            }
+        },
+        ngmin: {
+            main: {
+                src: ['src/application/main.js'],
+                dest: 'temp/main.js'
+            },
+            plangular: {
+                src: ['src/libs/plangular.js'],
+                dest: 'temp/plangular.js'
+            },
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'cordivacli-build/www/application/compiled.js': ['src/libs/jquery.min.js', 'src/libs/jquery-ui.custom.min.js', 'src/libs/fullcalendar.min.js', 'src/libs/gcal.js', 'src/libs/snap.min.js', 'src/libs/angular.min.js', 'src/libs/angular-bootstrap.min.js', 'src/libs/angular-route.min.js', 'src/libs/angular-snap.min.js', 'src/libs/angular-calendar.js', 'src/libs/angular-media-player.min.js', 'temp/plangular.js', 'temp/main.js']
+                }
+            }
+        },
+        compress: {
+            main: {
+                options: {
+                    mode: 'gzip'
+                },
+                files: [
+                    // Each of the files in the src/ folder will be output to
+                    // the dist/ folder each with the extension .gz.js
+                    {expand: true, cwd: 'temp/', src: ['compiled.js'], dest: 'cordivacli-build/www/application', ext: '.gz.js'},
+                    {expand: true, cwd: 'temp/', src: ['compiled.css'], dest: 'cordivacli-build/www/application', ext: '.gz.css'}
+                ]
+            }
+        },
+        cordovacli: {
+            init: {
+                options: {
+                    path: 'cordova-build'
+                },
+                cordova: {
+                    options: {
+                        command: ['create','platform','plugin','build'],
+                        platforms: ['android'],
+                        plugins: ['device','dialogs'],
+                        path: 'cordova-build',
+                        id: 'org.radioramadanedinburgh.app',
+                        name: 'Radio Ramadan'
+                    }
+                },
+                create: {
+                    options: {
+                        command: 'create',
+                        id: 'org.radioramadanedinburgh',
+                        name: 'app'
+                    }
+                },    
+                add_platforms: {
+                    options: {
+                        command: 'platform',
+                        action: 'add',
+                        platforms: ['android']
+                    }
+                },
+                add_plugins: {
+                    options: {
+                        command: 'plugin',
+                        action: 'add',
+                        plugins: ['media']
+                    }
+                },  
+            },    
+            build_android: {
+                options: {
+                    path: 'cordova-build'
+                },
+                build_android: {
+                    options: {
+                        command: 'build',
+                        platforms: ['android']
+                    }
+                },
+                emulate_android: {
+                    options: {
+                        command: 'emulate',
+                        platforms: ['android'],
+                        args: ['--target','Nexus_S']
+                    }
+                }
+            }
         }
-    },
-    cordovacli_init: {   
-        options: {
-            path: 'cordiva-build'
-        },
-        cordova: {
-            options: {
-                command: ['create','platform','plugin','build'],
-                platforms: ['android'],
-                plugins: ['device','dialogs'],
-                path: 'cordiva-build',
-                id: 'org.radioramadanedinburgh.app',
-                name: 'Radio Ramadan'
-            }
-        },
-        create: {
-            options: {
-                command: 'create',
-                id: 'org.radioramadanedinburgh',
-                name: 'app'
-            }
-        },    
-        add_platforms: {
-            options: {
-                command: 'platform',
-                action: 'add',
-                platforms: ['android']
-            }
-        },
-        add_plugins: {
-            options: {
-                command: 'plugin',
-                action: 'add',
-                plugins: ['media']
-            }
-        },  
-    },    
-    cordovacli_build_android: {
-        options: {
-            path: 'cordiva-build'
-        },
-        build_android: {
-            options: {
-                command: 'build',
-                platforms: ['android']
-            }
-        },
-        emulate_android: {
-            options: {
-                command: 'emulate',
-                platforms: ['android'],
-                args: ['--target','Nexus5']
-            }
-        }
-    }
-  });
-  // Load the plugins
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-processhtml');
-  grunt.loadNpmTasks('grunt-ngmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-cordovacli');
-  // Default tasks.
-  grunt.registerTask('default', ['clean', 'copy', 'ngmin', 'uglify', 'cssmin', 'processhtml']);
-  grunt.registerTask('mobile-init', ['cordovacli_init']);
-  grunt.registerTask('mobile-android', ['default', 'cordovacli_build_android']);
+    });
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-ngmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-cordovacli');
+    // Default tasks.
+    grunt.registerTask('default', ['clean', 'copy', 'ngmin', 'uglify', 'cssmin', 'processhtml']);
+    grunt.registerTask('mobile-init', ['cordovacli:init']);
+    grunt.registerTask('mobile-android', ['default', 'cordovacli:build_android']);
 };
